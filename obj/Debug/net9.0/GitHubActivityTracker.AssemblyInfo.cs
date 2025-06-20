ab88d352976297afa8b7d605c0d08e5b07d76fd3
David-Currey <davecurrey76@gmail.com>
2025-06-20 16:46:13 -0400

    screenshot

--- obj/Debug/net9.0/GitHubActivityTracker.AssemblyInfo.cs
+++ obj/Debug/net9.0/GitHubActivityTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GitHubActivityTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4496bda3df8e29303a84ff4ccaa27d924c5f53f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfd69ebcaaef13c8f5b5d3af3edcc209c2d259b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("GitHubActivityTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GitHubActivityTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
